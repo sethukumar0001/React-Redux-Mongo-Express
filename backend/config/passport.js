353bd4c2e88592d5a5868e4279a824836769ce85
sethukumar0001 <boylooksrock@gmail.com>
2019-09-18 21:57:05 +0530

    upto authentication

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,21 +1,24 @@
-const JWTStrategy = require('passport-jwt').Strategy;
-const ExtractJWT = require('passport-jwt').ExtractJwt;
-const mongoose = require('mongoose');
-const User = mongoose.model('users');
-const opts = {};
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+const keys = require("../config/keys");
 
-opts.jwtFromRequest = ExtractJWT.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = 'secret';
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
-    passport.use(new JWTStrategy(opts, (jwt_payload, done) => {
-        User.findById(jwt_payload.id)
-            .then(user => {
-                if(user) {
-                    return done(null, user);
-                }
-                return done(null, false);
-            })
-            .catch(err => console.error(err));
-    }));
-}
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
+};
